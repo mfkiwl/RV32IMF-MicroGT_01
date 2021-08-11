82bbd6cfd84e87298052eed1967cccd56426a8c6
Gabriele Tripi <68828254+GabbedT@users.noreply.github.com>
2021-08-11 14:27:57 +0200

    Update MGT-01_mul_tb.sv
    Changed the values tested.

--- Testbench/MGT-01_mul_tb.sv
+++ Testbench/MGT-01_mul_tb.sv
@@ -5,8 +5,8 @@ module MGT_01_mul_tb ();
   localparam T = 10;
   
   //Inputs
-  data_u           op_A_i;      //Multiplicand
-  data_u           op_B_i;      //Multiplier
+  data_u           multiplicand_i;      //Multiplicand
+  data_u           multiplier_i;        //Multiplier
 
   logic            clk_i;
   logic            clk_en_i;    //Clock enable to stall the pipelin
@@ -34,119 +34,51 @@ module MGT_01_mul_tb ();
     //Initial value
     initial 
       begin
-        op_A_i = 32'd9;
-        op_B_i = 32'd10;
+        multiplicand_i = 32'd1;
+        multiplier_i = 32'd1;
         clk_en_i = 1'b1;
-        ops_i = MUL_U;
+        ops_i = MUL_;
         #(4 * T);
       end
 
     //Stimuli
     initial 
       begin
-        op_A_i = 32'd9;
-        op_B_i = -32'd10;
-        ops_i = MUL_U;
+        //Test the pipeline introducing new input every cycle
+        multiplicand_i = 32'd9;
+        multiplier_i = 32'd10;
+        ops_i = MUL_;
+        
+        #T;
+        
+        multiplicand_i = -32'd12;
+        multiplier_i = 32'd10;
+        ops_i = MUL_;
+        
+        #T;
+        
+        multiplicand_i = -32'd100;
+        multiplier_i = 32'd10;
+        ops_i = MUL_;
+        
+        #T; 
+        
+        multiplicand_i = 32'd9;
+        multiplier_i = 32'd10;
+        ops_i = MUL_;
+        
+        //Every 4 cycles
+        multiplicand_i = 32'd1000;
+        multiplier_i = 32'd0;
+        ops_i = MUL_;
         
         #(4 * T);
         
-        op_A_i = 32'd45;
-        op_B_i = 32'd1;
-        ops_i = MUL_U;
+        multiplicand_i = 32'd10000;
+        multiplier_i = 32'd2;
+        ops_i = MUL_;
         
-        #(4 * T);
-        
-        op_A_i = 32'd91234;
-        op_B_i = 32'd102345;
-        ops_i = MULH_U;
-        
-        #(6 * T);
-
-//        op_A_i = 32'd90;
-//        op_B_i = 32'd10;
-//        ops_i = MUL_U;
-
-//        #T;
-
-//        op_A_i = -32'd9;
-//        op_B_i = 32'd10;
-//        ops_i = MUL_U;
-
-//        #T;
-
-//        op_A_i = -32'd9;
-//        op_B_i = -32'd10;
-//        ops_i = MUL_U;
-
-//        #T;
-
-//        op_A_i = 32'd9;
-//        op_B_i = 32'd10;
-//        ops_i = MULH_U;
-
-//        #T;
-
-//        clk_en_i = 1'b0;
-
-//        #(2 * T);
-
-//        clk_en_i = 1'b1;
-
-//        #T;
-
-//        op_A_i = 32'd9235;
-//        op_B_i = 32'd1000;
-//        ops_i = MULH_U;
-
-//        #T;
-
-//        op_A_i = -32'd9235;
-//        op_B_i = 32'd1000;
-//        ops_i = MULH_U;
-
-//        #T;
-
-//        op_A_i = -32'd9235;
-//        op_B_i = -32'd1000;
-//        ops_i = MULH_U;
-
-//        #T;
-
-//        op_A_i = -32'd9;
-//        op_B_i = 32'd10;
-//        ops_i = MULHSU_U;
-
-//        #T;
-
-//        op_A_i = 32'd9;
-//        op_B_i = -32'd10;
-//        ops_i = MULHSU_U;
-
-//        #T;
-
-//        op_A_i = -32'd9;
-//        op_B_i = -32'd10;
-//        ops_i = MULHSU_U;
-
-//        #T;
-
-//        op_A_i = 32'd9;
-//        op_B_i = -32'd10;
-//        ops_i = MULHU_U;
-
-//        #T;
-
-//        op_A_i = -32'd9;
-//        op_B_i = -32'd10;
-//        ops_i = MULHU_U;
-
-//        #T;
-
-//        op_A_i = 32'd9;
-//        op_B_i = 32'd10;
-//        ops_i = MULHU_U;
-
-//        #T;
+        #(5 * T)
         $stop;
       end
 
