d35fe79cebb8c661e77c6e611aa69693b2b5e767
Gabriele Tripi <68828254+GabbedT@users.noreply.github.com>
2021-08-19 11:52:02 +0200

    Update MGT-01_multiplier_unit_tb.sv

--- Testbench/MGT-01_multiplier_unit_tb.sv
+++ Testbench/MGT-01_multiplier_unit_tb.sv
@@ -1,6 +1,9 @@
 `timescale 1ns/1ps
 
-module MGT_01_booth_radix4_tb ();
+`include "Instruction_pkg.svh"
+`include "Modules_pkg.svh"
+
+module MGT_01_multiply_unit_tb ();
   
   localparam T = 10;
 
@@ -9,14 +12,22 @@ module MGT_01_booth_radix4_tb ();
 
   logic                            clk_i, clk_en_i;               //Clock signals
   logic                            rst_n_i;                       //Reset active low
+
+  mul_ops_e                        operation_i;
   
   //Outputs
   logic signed [XLEN - 1:0]        result_o;                      
   fu_state_e                       fu_state_o;                    //Functional unit state
+    
 
-  MGT_01_booth_radix4 uut (.*);
-
+  MGT_01_multiply_unit uut (.*);
+  
+  //TB signal
+  logic signed [(XLEN * 2) - 1:0]  result_64;
+  
   //Test
+  
+    assign result_64 = multiplier_i * multiplicand_i;
 
     //Clock 
     always 
@@ -31,34 +42,44 @@ module MGT_01_booth_radix4_tb ();
       begin
         rst_n_i = 1'b0;
         clk_en_i = 1'b1;
+        operation_i = MUL_;
         multiplier_i = 32'd0;
-        multiplicand_i = 32'd0;
+        multiplicand_i = 32'd0; 
         
         #(2 * T);
         
         rst_n_i = 1'b1;
         clk_en_i = 1'b1;
+        operation_i = MUL_;
         multiplier_i = 32'd10;
-        multiplicand_i = 32'd20;        
+        multiplicand_i = 32'd20;      
+
         #(17 * T);
         
         multiplier_i = 32'd100;
         multiplicand_i = -32'd200;      
-        
+        operation_i = MUL_;
+            
         #(17 * T);
         
         multiplier_i = 32'd150;
         multiplicand_i = 32'd1000;      
-        
+        operation_i = MUL_;
+      
         #(17 * T);
-        
+               
         multiplier_i = -32'd10;
         multiplicand_i = -32'd20;       
-        #(17 * T);
+        operation_i = MULH_;
         
+        #(17 * T);
+               
         multiplier_i = 32'hFFFFFFFF;
         multiplicand_i = 32'h80000000;  
+        operation_i = MULHU_;
+
         #(17 * T);
+        
 
         $stop;
       end
