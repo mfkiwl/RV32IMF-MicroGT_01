b4b412e9844de78c2dcec7b5a77a924822950d4d
Gabriele Tripi <68828254+GabbedT@users.noreply.github.com>
2021-08-19 19:38:46 +0200

    Update and rename MGT-01_multiplier_unit.sv to MGT-01_mul_unit.sv

--- RTL/Arithmetic Modules/MGT-01_mul_unit.sv
+++ RTL/Arithmetic Modules/MGT-01_mul_unit.sv
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Creator:        Gabriele Tripi - gabrieletripi02@gmail.com                 //
 //                                                                            //
-// Design Name:    Multiply Unit                                              //
+// Design Name:    Multiplication Unit                                        //
 // Project Name:   MicroGT-01                                                 //
 // Language:       SystemVerilog                                              //
 //                                                                            //
