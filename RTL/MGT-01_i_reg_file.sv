365f84a168fa79df0b9ac1ef72df3f3c3ca147b7
Gabriele Tripi <68828254+GabbedT@users.noreply.github.com>
2021-09-07 01:13:07 +0200

    Update MGT-01_i_reg_file.sv

--- RTL/MGT-01_i_reg_file.sv
+++ RTL/MGT-01_i_reg_file.sv
@@ -9,30 +9,30 @@
 //                 integer registers. It has 2 read and 1 write ports.        //
 ////////////////////////////////////////////////////////////////////////////////
 
-`include "Modules_pkg.svh"
-`include "Instruction_pkg.svh"
+`include "Primitives/Modules_pkg.svh"
+`include "Primitives/Instruction_pkg.svh"
 
 module MGT_01_i_reg_file 
-(   //Inputs
-    input  logic        clk_i,       //Clock 
-    input  logic        we_i,        //Write enable 
-    input  logic        clk_en_i,
+( //Inputs
+  input  logic        clk_i,       //Clock 
+  input  logic        we_i,        //Write enable 
+  input  logic        clk_en_i,
 
-    input  i_register_e r1_iaddr_i,  //Read addresses
-    input  i_register_e r2_iaddr_i,
+  input  i_register_e r1_iaddr_i,  //Read addresses
+  input  i_register_e r2_iaddr_i,
 
-    input  i_register_e w_iaddr_i,   //Write address
+  input  i_register_e w_iaddr_i,   //Write address
 
-    input  data_bus_t   wr_idata_i,  //Write port
+  input  data_bus_t   wr_idata_i,  //Write port
 
-    //Outputs
-    output data_bus_t   r1_idata_o,  //Read ports
-    output data_bus_t   r2_idata_o
+  //Outputs
+  output data_bus_t   r1_idata_o,  //Read ports
+  output data_bus_t   r2_idata_o
 );
 
-    data_bus_t i_REG_FILE [0:XLEN - 1];   //Register file 
+  data_bus_t i_REG_FILE [0:XLEN - 1];   //Register file 
     
-    logic we;
+  logic we;
     
     assign we = (w_iaddr_i == X0) ? 1'b0 : we_i;  //If write address is register x0 don't write 
 
@@ -42,9 +42,9 @@ module MGT_01_i_reg_file
           i_REG_FILE[w_iaddr_i] <= wr_idata_i;  //Write on positive edge of clk
       end
 
-    //Reads are combinatorials
-    assign r1_idata_o = (r1_iaddr_i == X0) ? 32'b0 : i_REG_FILE[r1_iaddr_i];   //Register x0 is hardwired to 0
+  //Reads are combinatorials
+  assign r1_idata_o = (r1_iaddr_i == X0) ? 32'b0 : i_REG_FILE[r1_iaddr_i];   //Register x0 is hardwired to 0
 
-    assign r2_idata_o = (r2_iaddr_i == X0) ? 32'b0 : i_REG_FILE[r2_iaddr_i];
+  assign r2_idata_o = (r2_iaddr_i == X0) ? 32'b0 : i_REG_FILE[r2_iaddr_i];
 
 endmodule
