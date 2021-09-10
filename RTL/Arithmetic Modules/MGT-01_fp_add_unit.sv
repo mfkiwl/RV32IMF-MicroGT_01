1f3761213e6f8530d50ab575d14d2b2ea50b6d3c
Gabriele Tripi <68828254+GabbedT@users.noreply.github.com>
2021-09-10 14:57:50 +0200

    Update MGT-01_fp_add_unit.sv

--- RTL/Arithmetic Modules/MGT-01_fp_add_unit.sv
+++ RTL/Arithmetic Modules/MGT-01_fp_add_unit.sv
@@ -10,8 +10,8 @@
 //                 a valid result in 4 clock cycles.                          //
 ////////////////////////////////////////////////////////////////////////////////
 
-`include "Primitives/Modules_pkg.svh"
-`include "Primitives/Instruction_pkg.svh"
+`include "Modules_pkg.svh"
+`include "Instruction_pkg.svh"
 
 module MGT_01_fp_add_unit
 ( //Inputs
