6fa68f7800a0df5587b063429cbd9f530366dd9e
Gabriele Tripi <68828254+GabbedT@users.noreply.github.com>
2021-09-15 22:43:36 +0200

    Update MGT-01_nr_divider.sv

--- RTL/Arithmetic Modules/MGT-01_nr_divider.sv
+++ RTL/Arithmetic Modules/MGT-01_nr_divider.sv
@@ -11,8 +11,8 @@
 
 //NOT TESTED
 
-`include "Primitives/Modules_pkg.svh"
-`include "Primitives/Instruction_pkg.svh"
+`include "Modules_pkg.svh"
+`include "Instruction_pkg.svh"
 
 module MGT_01_nr_divider 
 ( //Inputs
