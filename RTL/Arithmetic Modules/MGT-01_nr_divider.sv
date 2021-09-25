1dbd88889ef6eb8a9b18660a2864e8b33fc7a09f
Gabriele Tripi <68828254+GabbedT@users.noreply.github.com>
2021-09-25 17:45:16 +0200

    Update MGT-01_nr_divider.sv

--- RTL/Arithmetic Modules/MGT-01_nr_divider.sv
+++ RTL/Arithmetic Modules/MGT-01_nr_divider.sv
@@ -9,32 +9,35 @@
 //                 division. It is used for the floating point division.      //
 ////////////////////////////////////////////////////////////////////////////////
 
-//NOT TESTED
-
 `include "Modules_pkg.svh"
 `include "Instruction_pkg.svh"
 
-module MGT_01_nr_divider 
-( //Inputs
-  input  logic signed [XLEN - 1:0]        dividend_i, divisor_i, 
+module MGT_01_nr_divider ( 
+  //Inputs
+  input  logic signed [24:0]              dividend_i, divisor_i, 
 
   input  logic                            clk_i, clk_en_i,               //Clock signals
   input  logic                            rst_n_i,                       //Reset active low
 
   //Outputs
-  output logic signed [XLEN - 1:0]        quotient_o,                      
+  output logic signed [24:0]              quotient_o, remainder_o,                     
   output logic                            valid_o,
   output logic                            zero_divide_o
 );
 
+  //Number of iteration done by the sequential divider 
+  localparam ITERATIONS = 24;
+
+  //XLEN == 24 XLEN - 1 == 23
+
   typedef enum logic [1:0] {IDLE, DIVIDE, RESTORING, VALID} fsm_state_e;
 
   typedef struct packed {
-      logic signed [XLEN:0]      _P;      //Partial 
-      logic signed [XLEN - 1:0]  _A;      //Dividend
+      logic signed [25:0] _P;      //Partial 
+      logic signed [24:0] _A;      //Dividend
   } reg_pair_s;
 
-  logic signed [XLEN - 1:0] dividend, divisor;
+  logic signed [24:0] dividend, divisor;
 
   assign dividend = dividend_i;
 
@@ -43,7 +46,7 @@ module MGT_01_nr_divider
   //Current and next FSM state
   fsm_state_e crt_state, nxt_state;
 
-  logic [4:0] counter;
+  logic [5:0] counter;
   
   logic rst_n_dly;  //Reset delayed
 
@@ -67,10 +70,10 @@ module MGT_01_nr_divider
       //Counter, it tracks the state of the operation
       always_ff @(posedge clk_i)
         begin : COUNTER
-          if (!rst_n_i)
+          if (!rst_n_i | (counter == ITERATIONS))
             counter <= 0;
           else if (clk_en_i && (crt_state == DIVIDE))
-            counter <= counter + 1;
+            counter <= counter + 1; 
         end : COUNTER
 
       //Next state logic
@@ -80,7 +83,7 @@ module MGT_01_nr_divider
 
             IDLE:       nxt_state = (~rst_n_dly) ? IDLE : DIVIDE;
 
-            DIVIDE:     nxt_state = (&counter) ? RESTORING : DIVIDE;  //If counter is equal to 11111
+            DIVIDE:     nxt_state = (counter == ITERATIONS) ? RESTORING : DIVIDE;  //If counter is equal to 11111
 
             RESTORING:  nxt_state = VALID;
 
@@ -93,22 +96,22 @@ module MGT_01_nr_divider
   reg_pair_s reg_pair_in, reg_pair_out;
 
   //Shifted dividend
-  logic signed [XLEN - 1:0] A_shifted;
+  logic signed [24:0] A_shifted;
 
   //Partial division
-  logic signed [XLEN:0] partial_division, partial_division_shift;
+  logic signed [25:0] partial_division, partial_division_shift;
 
   //Divisor register nets
-  logic signed [XLEN:0] reg_b_in, reg_b_out;
+  logic signed [25:0] reg_b_in, reg_b_out;
 
-  assign reg_b_in = {divisor[XLEN - 1], divisor};
+  assign reg_b_in = {divisor[24], divisor};
 
       //Data registers
 
       always_ff @(posedge clk_i)
         begin : REG_B 
           if (!rst_n_i)
-            reg_b_out <= 32'b0;
+            reg_b_out <= 26'b0;
           else if ((crt_state == IDLE) && clk_en_i)    //Don't update the register until the operation is completed
             reg_b_out <= reg_b_in;
         end : REG_B 
@@ -120,7 +123,7 @@ module MGT_01_nr_divider
           if (clk_en_i)
             begin
               if (crt_state == IDLE)
-                reg_pair_out <= '{33'b0, dividend};
+                reg_pair_out <= '{dividend, 26'b0};
               else if (crt_state == DIVIDE)
                 reg_pair_out <= reg_pair_in;
               else 
@@ -128,22 +131,25 @@ module MGT_01_nr_divider
             end
         end : REG_P_A 
 
-  //Algorithm logic
+  /////////////////////
+  // Algorithm logic //
+  /////////////////////
 
-  assign {partial_division_shift, A_shifted} = reg_pair_out << 1; //Shift left one
+  //If the divider is in the first iteration just skip it
+  assign {partial_division_shift, A_shifted} = (~|counter) ? reg_pair_out : (reg_pair_out << 1); 
         
-  assign reg_pair_in._A = {A_shifted[XLEN - 1:1], ~reg_pair_out._P[XLEN]};    //If P[XLEN] == 1 => A[0] = 0 else A[0] = 1;
+  assign reg_pair_in._A = {A_shifted[24:1], ~partial_division[25]};    //If P[XLEN] == 1 => A[0] = 0 else A[0] = 1;
   assign reg_pair_in._P = partial_division;
 
       always_comb 
         begin : DIVISION_LOGIC
           if (crt_state == IDLE)
             begin
-              partial_division = 32'b0; //Initialize to zero 
+              partial_division = 26'b0; //Initialize to zero 
             end
           else if (crt_state == DIVIDE)
             begin
-              if (reg_pair_out._P[XLEN] == 1'b1)  //If P is negative
+              if (reg_pair_out._P[25] == 1'b1)  //If P is negative
                 begin                  
                   partial_division = partial_division_shift + reg_b_out;  //Add divisor to P
                 end
@@ -154,25 +160,21 @@ module MGT_01_nr_divider
             end
           else if (crt_state == RESTORING)
             begin
-              if (reg_pair_out._P[XLEN] == 1'b1)     //If P is negative
+              if (reg_pair_out._P[25] == 1'b1)     //If P is negative
                 partial_division = reg_pair_out._P + reg_b_out;
-              else                                    //If P is positive
-                partial_division = reg_pair_out._P + 32'b0;
+              else                                   //If P is positive
+                partial_division = reg_pair_out._P + 0;
             end
           else //DEFAULT
-            partial_division = 32'b0;
-
+            partial_division = reg_pair_out._P;
         end : DIVISION_LOGIC
 
-  //NOR of divisor 
-  logic zero_divide;
+  
+  assign zero_divide_o = ~|divisor_i;
 
-  assign zero_divide = ~|divisor_i;
+  assign quotient_o = reg_pair_out._A;
+  assign remainder_o = reg_pair_out._P[24:0];
 
-  assign zero_divide_o = zero_divide;
-
-  assign quotient_o = A_shifted;
-
-  assign valid_o = (crt_state == VALID) & (~zero_divide);
+  assign valid_o = (crt_state == VALID);
 
 endmodule
